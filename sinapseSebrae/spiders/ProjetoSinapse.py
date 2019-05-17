68bffeb277571f4cd34b9575fdc892fa6c3bfa62
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2019-05-17 01:11:53 -0300

    [REFACT] adicionando funcoes para parse #2
    Foram adicionadas as funcoes:
      get_data_from_div_media_body
      get_data_from_div_content
      get_data_from_div_sidebar
    
    Funcoes especializadas em realizar o parse dos dados.
    Podem ser adicionadas algumas melhorias para que se uma funÃÃo gere
    exceÃÃo, nÃo atrapalhe a execuÃÃo das demais.
    Algum log seria interessante para colocar em qual url de projeto gerou
    uma exceÃÃo e nÃ£o foi possÃvel capturar os dados.
    
    TambÃm Ã© necessÃrio refatorar o cÃdigo que captura o dado das Ãltimas
    atualizaÃÃes.
    
     On branch refatorar-parse-projeto
     Changes to be committed:
    	deleted:    projetos.json
    	new file:   saida.json
    	modified:   sinapseSebrae/spiders/ProjetoSinapse.py

--- sinapseSebrae/spiders/ProjetoSinapse.py
+++ sinapseSebrae/spiders/ProjetoSinapse.py
@@ -8,11 +8,11 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-from sinapseSebrae.items import ProjetoItem
-
 from bs4 import BeautifulSoup
-import time
-import os
+import bs4
+from time import sleep
+from os import path
+from re import split as re_split
 
 
 class ProjetoSinapseSpider(scrapy.Spider):
@@ -52,7 +52,7 @@ class ProjetoSinapseSpider(scrapy.Spider):
             qtd_total = int(qtd_total)
             
             for i in range(2,qtd_total):
-                time.sleep(5)
+                sleep(5)
                 pagination = self.browser.find_element_by_css_selector('ul.pagination')
                 caminho = f'//*/a[contains(text(),\'{i}\')]'
                 a = pagination.find_element_by_xpath(caminho)
@@ -68,38 +68,22 @@ class ProjetoSinapseSpider(scrapy.Spider):
     def parse_project_page(self,response):
         page = response.url.split("/")[-1]
         filename = 'sinapse-%s.html' % page
-        diretorio = os.path.join('data','web',filename)
+        diretorio = path.join('data','web',filename)
         with open(diretorio, 'wb') as f:
             f.write(response.body)
 
         soup = BeautifulSoup(response.body,'html.parser',from_encoding="utf-8")
-        content = soup.find_all("div",{"class":"content"})[0]
-
-        children = content.select("h3, p")
-        projeto = ProjetoItem()
-        projeto['titulo'] = soup.title.string.strip().strip('.').replace('Sinapse da Inovação -','').strip()
+        
+        projeto = dict()
         projeto["url_projeto"] = response.url
 
-        item = children[0]
-        if item.get_text().strip() == "Descrição do problema":
-            item = children[1]
-            projeto['problema'] = item.get_text().strip()
-        
-        item = children[2]
-        if item.get_text().strip() == "Solução Proposta":
-            item = children[3]
-            projeto["proposta"] = item.get_text().strip('[\n\t]').strip()
-
-        observacoes = list()
-        for i in range(4,len(children)):
-            item = children[i]
-            observacoes.append(item.get_text().strip())
-
-        projeto["observacoes"] = "  ".join(observacoes)
+        projeto.update(get_data_from_div_media_body(soup))
+        projeto.update(get_data_from_div_content(soup))
+        projeto.update(get_data_from_div_sidebar(soup))
 
         texto = soup.find('div','well').get_text().strip()
         indice = texto.rfind('em ')
-        projeto["atualizacao"] = texto[indice:]
+        projeto["atualizado"] = texto[indice:]
 
         yield projeto
 
@@ -112,8 +96,82 @@ class ProjetoSinapseSpider(scrapy.Spider):
 
         for p in div:            
             for _, child in enumerate(p.findChildren('a')):                                
-                if 'media' in child.get('class',[]):
+                if 'media' in child.get('class','-'):
                     url_idea = url_root + child['href']                                        
                     list_urls.append(url_idea)
 
         return list_urls
+
+
+def get_data_from_div_media_body(soup):
+    data = dict()
+    data.setdefault('comentarios','0')
+    data.setdefault('favoritos','0')
+    div = soup.find('div',attrs={'class' : 'media-body'})
+    data['titulo'] = div.h1.string
+    data['categoria'] = div.a.string
+
+    for element in div.find_all('span'):
+        if isinstance(element,bs4.element.Tag):
+                if element.string == 'Setores:':
+                    tmp = element.next_sibling.strip('[\n\t ]+')
+                    data['setor'] = tmp.split('/')
+                elif element.string == 'Região':
+                    data['regiao'] = element.next_sibling.strip('[\n\t ]+')
+                elif element.get('title') == 'Favoritada':
+                    if element.text.strip():
+                        data['favoritos'] = element.text.strip()
+
+    link = div.find('a',attrs={'class': 'count'})
+    if link.text.strip():
+        data['comentarios'] = link.text.strip()
+
+    return data
+
+def get_data_from_div_content(soup):
+    data = dict()
+    div = soup.find('div',attrs={'class':'content'})
+    children = div.select('h3, p')
+
+    schema = {
+        'Descrição do problema' : 'problema',
+        'Solução Proposta' : 'proposta',
+        'Estágio de desenvolvimento' : 'estagio',
+        'Diferenciais da Solução' : 'diferencial',
+        'Políticas públicas' : 'politicas'
+    }
+    for e in children:
+        if e.name == 'h3':
+            key = schema.get(e.string.strip(),'outros')
+        elif e.name == 'p':
+            if data.get(key,False):
+                ## key already exists
+                data[key] += e.text.strip('[\n\t ]+')
+            else :
+                ## key não existe previamente
+                data[key] = e.text.strip('[\n\t ]+')
+
+    return data
+
+
+def get_data_from_div_sidebar(soup):
+    data = dict()
+
+    div = soup.find('div',attrs={'class':'sidebar'})
+    text = div.text.strip('[\n\t ]+')
+    texts = re_split('[\n\t]+',text)
+    sobre, cidade, nome, *resto = texts
+    del sobre
+
+    data['cidade'] = cidade.replace('Cidade:','').strip()
+    data['nome'] = nome
+    resto = ' '.join(resto)
+    resto = resto.replace('pt_BR','').replace('Compartilhe essa Ideia','').replace('Tweetar','').strip()
+    data['sobre'] = resto
+
+    return data
+
+
+
+
+
